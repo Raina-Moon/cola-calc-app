a5de8c8a4649352e9034b8a4974f34943ed7ea3f
Raina Moon <mds6425@gmail.com>
2025-05-12 20:38:50 +0900

    ✨ add : implement bubble animation and splash screen functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,133 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Dimensions, Image, StyleSheet, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useRouter } from "expo-router";
+
+const { width } = Dimensions.get("window");
+
+interface Bubble {
+  id: number;
+  x: number;
+  size: number;
+  anim: Animated.Value;
+  opacity: Animated.Value;
+}
 
 export default function Index() {
+  const [ready, setReady] = useState(false);
+  const [bubbles, setBubbles] = useState<Bubble[]>([]);
+  const scaleAnim = new Animated.Value(0);
+  const bubbleId = useRef(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const startAnimation = async () => {
+      Animated.timing(scaleAnim, {
+        toValue: 1,
+        duration: 800,
+        useNativeDriver: true,
+      }).start(async () => {
+        await SplashScreen.hideAsync();
+        setReady(true);
+
+        setTimeout(() => {
+          router.replace("/home")
+        },1000)
+      });
+    };
+    startAnimation();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      addBubble();
+      addBubble();
+    }, 180);
+    return () => clearInterval(interval);
+  }, []);
+
+  const addBubble = () => {
+    const id = bubbleId.current++;
+    const size = Math.random() * 20 + 10;
+    const logoCenterX = width / 2;
+    const x = logoCenterX + Math.random() * 200 - 100;
+
+    const anim = new Animated.Value(0);
+    const opacity = new Animated.Value(1);
+
+    const newBubble: Bubble = {
+      id,
+      x,
+      size,
+      anim,
+      opacity,
+    };
+    setBubbles((prev) => [...prev, newBubble]);
+
+    Animated.parallel([
+      Animated.timing(anim, {
+        toValue: -150,
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      setBubbles((prev) => prev.filter((b) => b.id !== id));
+    });
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      {bubbles.map((bubble) => (
+        <Animated.View
+          key={bubble.id}
+          style={[
+            styles.bubble,
+            {
+              width: bubble.size,
+              height: bubble.size,
+              left: bubble.x,
+              opacity: bubble.opacity,
+              transform: [{ translateY: bubble.anim }],
+            },
+          ]}
+        />
+      ))}
+      <Image
+        source={require(`../assets/images/sipsense.png`)}
+        style={styles.logo}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  logo: {
+    width: 300,
+    height: 150,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [
+      {
+        translateX: -150,
+      },
+      { translateY: -150 },
+    ],
+  },
+  bubble: {
+    position: "absolute",
+    backgroundColor: "rgba(121, 47, 47, 0.5)",
+    borderRadius: 50,
+  },
+});
