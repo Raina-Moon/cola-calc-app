a705b85d0ca81be287b4a315e88533340638242b
Raina Moon <mds6425@gmail.com>
2025-05-14 20:22:56 +0900

    ✨ feat: implement axios instance with request and response interceptors for authentication

---
+++ app/api/axios.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useAuthStore } from "../store/authStore";
+import { refreshAccessToken } from "./auth";
+
+const API = axios.create({ baseURL: "http://54.161.66.184/5000/api" });
+
+API.interceptors.request.use(
+  async (config) => {
+    const token = useAuthStore.getState().accessToken;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.resonse?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newAccessToken = await refreshAccessToken();
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return API(originalRequest);
+      } catch (err) {
+        await useAuthStore.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default API;
