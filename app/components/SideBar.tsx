659702e65ee7694baba55b27d62085d2fe393fc3
Raina Moon <mds6425@gmail.com>
2025-05-16 20:32:08 +0900

    💄 ui: refactor SideBar component to remove profile option and enhance user greeting

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,18 +1,18 @@
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useAuthStore } from "../store/authStore";
 
 const SideBar = () => {
   const [selected, setSelected] = useState("home");
   const router = useRouter();
+
+  const userName = useAuthStore((state) => state.user?.name);
   const handleSelect = (item: string) => {
     setSelected(item);
     switch (item) {
       case "home":
         router.replace("/home");
-        break;
-      case "profile":
-        router.replace("/profile");
         break;
       case "settings":
         router.replace("/settings");
@@ -27,12 +27,17 @@ const SideBar = () => {
 
   const itemList = [
     { id: "home", name: "Home" },
-    { id: "profile", name: "Profile" },
     { id: "settings", name: "Settings" },
     { id: "logout", name: "Logout" },
   ];
   return (
     <View>
+      <TouchableOpacity onPress={() => router.replace("/profile")}>
+        <Text>
+          Hi!
+          <Text style={{ textDecorationLine: "underline" }}>{userName}</Text>
+        </Text>
+      </TouchableOpacity>
       {itemList.map((item) => (
         <View key={item.id}>
           <Text onPress={() => handleSelect(item.id)}>{item.name}</Text>
