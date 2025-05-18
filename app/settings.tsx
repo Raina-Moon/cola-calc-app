d6390b8b16f35cf2cad5b50af949241cfd34e58e
Raina Moon <mds6425@gmail.com>
2025-05-19 00:14:01 +0900

    💄ui: refactor settings component layout and styling for improved user experience

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect, useState } from "react";
-import { Switch, Text, View } from "react-native";
+import { StyleSheet, Switch, Text, View } from "react-native";
 import { useAuthStore } from "./store/authStore";
 import { toggleNotif } from "./api/notification";
 
@@ -28,9 +28,11 @@ const settings = () => {
   };
 
   return (
-    <View>
-      <Text style={{ fontSize: 18, marginBottom: 10 }}>
-        Toggle Notifications!
+    <View style={styles.container}>
+      <Text style={styles.header}>Settings</Text>
+      <View style={styles.card}>
+      <Text style={styles.label}>
+        🔔 Enable Notification
       </Text>
       <Switch
         value={enabled}
@@ -38,8 +40,46 @@ const settings = () => {
         thumbColor={enabled ? "#fff" : "#ccc"}
         trackColor={{ false: "#888", true: "#4CAF50" }}
       />
+      </View>
     </View>
   );
 };
 
 export default settings;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#ff4747",
+  },
+  header : {
+    fontSize: 32,
+    marginVertical: 20,
+    color: "#fff",
+    fontFamily: "Jersey15_400Regular",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: "90%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label : {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "#141414",
+    fontFamily: "Jersey15_400Regular",
+  }
+})
